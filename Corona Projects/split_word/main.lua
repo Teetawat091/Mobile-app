6491ad43b43ba4835b98f5fd2c8b1e81450f273e
Teetawat Chitpitak <sandna03@gmail.com>
2016-11-11 18:38:50 +0700

    complete split-word

--- Corona Projects/split_word/main.lua
+++ Corona Projects/split_word/main.lua
@@ -5,3 +5,31 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+s = "thai lao singapore"
+words = {}
+
+for word in s:gmatch("%w+")do
+table.insert(words,word)
+end
+
+for i = 1, #words do
+
+print(words[i])
+end
+
+alert = native.showAlert(
+"Test-split-words",
+"ข้อความที่ถูกแบ่ง",
+words,
+showalertListener
+)
+
+function showalertListener (event)
+
+if(event.action == "clicked")then
+print(event.index)
+print(words[event.index])
+end
+end
+
